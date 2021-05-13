a88c6d2bc24d8f5a56e720e57c1b3124e7129e9b
fireworkmarks <806229693@qq.com>
2021-05-13 09:22:51 +0800

    feat: remove target folder

--- learning-package/guessing_game/target/debug/build/getrandom-840dfc75a010aa1f/build_script_build-840dfc75a010aa1f.d
+++
@@ -1,5 +0,0 @@
-D:\code\rust-test\learning-package\guessing_game\target\debug\build\getrandom-840dfc75a010aa1f\build_script_build-840dfc75a010aa1f.exe: C:\Users\MSI_NB\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.2\build.rs
-
-D:\code\rust-test\learning-package\guessing_game\target\debug\build\getrandom-840dfc75a010aa1f\build_script_build-840dfc75a010aa1f.d: C:\Users\MSI_NB\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.2\build.rs
-
-C:\Users\MSI_NB\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.2\build.rs:
--- learning-package/guessing_game/target/debug/deps/guessing_game.d
+++
@@ -1,5 +0,0 @@
-D:\code\rust-test\learning-package\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
-
-D:\code\rust-test\learning-package\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
-
-src\main.rs:
