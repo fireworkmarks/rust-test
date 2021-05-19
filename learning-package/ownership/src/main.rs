a6184a1f740b0c504ac0e46ce83925af81ec9a8f
fireworkmarks <806229693@qq.com>
2021-05-19 09:37:02 +0800

    feat: add ownership of basic type

--- learning-package/ownership/src/main.rs
+++ learning-package/ownership/src/main.rs
@@ -2,12 +2,20 @@ fn main() {
     let s1 = String::from("Hello");
 
     /*
-        if use s1 not have clone, IDE will tell you panic. It ownership will change to s2, so s1
+        If use s1 not have clone, IDE will tell you panic. It ownership will change to s2, so s1
         will be gc.
 
         let s2 = s1; wrong!
      */
     let s2 = s1.clone();
 
-    println!("{}, {}!", s1, s2)
+    println!("{}, {}!", s1, s2);
+
+    /*
+         Here not need memory copy, they are basic type.
+     */
+    let a1 = 3;
+    let a2 = a1;
+
+    println!("{}, {}!", a1, a2)
 }
